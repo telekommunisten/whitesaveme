24ffdc7917bcb86b372d9cf38c22c6e16c71b640
Dmytri Kleiner <dk@trick.ca>
2015-06-22 19:05:49 +0200

    cleaning up from lots of tests

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: node node_modules/http-server/bin/http-server site/
+web: node node_modules/http-server/bin/http-server site/ -c-1 -p $PORT
--- site/lib/run.js
+++ site/lib/run.js
@@ -1,14 +1,29 @@
 "use strict"
 
-var $ = require('jquery');
-require('materialize');
+import $ from 'jquery';
+import riot from 'riot';
+import 'materialize';
+import 'style.css!'
 
-require('style.css!css');
+//import riotNav from 'layout/riot-nav.html!text';
 
-jQuery(document).ready(function () {
-  document.getElementsByTagName('body')[0]
-    .innerHTML = require('../pages/hello.html!text');
-  require('lib/init');
+$(document).ready(function () {
+//  var loadTag = function(tag) {
+//    var s = document.createElement('script');
+//    s.type = 'riot/tag';
+//    $(s).append(tag);
+//    $('body').append(s);
+//  }
+//  loadTag(riotNav);
+//  riot.mount('riot-nav', {});
+//  document.getElementsByTagName('content')[0]
+//    .innerHTML = require('../pages/home.html!text');
+//  require('lib/init');
+//  riot.route(function(page) {
+//    document.getElementsByTagName('content')[0] 
+//      .innerHTML = require('../pages/hello.html!text');
+//      console.log(page);
+//  });
 });
 
 
