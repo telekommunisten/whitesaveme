d8e25f0b82c6cd46eb8f581abc4eca45379f992a
Dmytri Kleiner <dk@trick.ca>
2015-06-23 09:39:49 +0200

    :smile:

--- site/config.js
+++ site/config.js
@@ -1,35 +1,17 @@
 System.config({
-  "transpiler": "babel",
-  "babelOptions": {
-    "optional": [
-      "runtime"
-    ]
-  },
+  "transpiler": "traceur",
   "paths": {
     "*": "*.js",
-    "github:*": "jspm_packages/github/*.js",
-    "npm:*": "jspm_packages/npm/*.js"
+    "github:*": "jspm_packages/github/*.js"
   }
 });
 
 System.config({
   "map": {
-    "babel": "npm:babel-core@5.6.4",
-    "babel-runtime": "npm:babel-runtime@5.6.4",
-    "core-js": "npm:core-js@0.9.18",
     "css": "github:systemjs/plugin-css@0.1.13",
     "text": "github:systemjs/plugin-text@0.0.2",
-    "github:jspm/nodelibs-process@0.1.1": {
-      "process": "npm:process@0.10.1"
-    },
-    "npm:babel-runtime@5.6.4": {
-      "process": "github:jspm/nodelibs-process@0.1.1"
-    },
-    "npm:core-js@0.9.18": {
-      "fs": "github:jspm/nodelibs-fs@0.1.2",
-      "process": "github:jspm/nodelibs-process@0.1.1",
-      "systemjs-json": "github:systemjs/plugin-json@0.1.0"
-    }
+    "traceur": "github:jmcriffey/bower-traceur@0.0.88",
+    "traceur-runtime": "github:jmcriffey/bower-traceur-runtime@0.0.88"
   }
 });
 
