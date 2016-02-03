015e75047dbfd07fac7362631e77526d8c8c6b30
Dmytri Kleiner <dk@trick.ca>
2016-02-03 18:01:48 +0100

    oh boy

--- build.sh
+++ build.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 node_modules/.bin/jspm install
-#node_modules/.bin/jspm bundle-sfx 'lib/init'
 node_modules/.bin/jspm bundle-sfx 'lib/init'
-node_modules/.bin/uglifyjs site/build.js -vo site/whitesaveme.js
-#uglify -s site/build.js -o site/whitesaveme.js
-#uglify -cs site/whitesaveme.css -o site/whitesaveme.min.css
+cp site/build.js site/whitesaveme.js
+cp site/whitesaveme.css site/whitesaveme.min.css
 cp site/bundle.html site/index.html
